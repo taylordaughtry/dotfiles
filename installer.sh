876f73067bf8bc771ef28b4a67024b2756539b2f
Taylor Daughtry <hello@taylordaughtry.com>
2017-01-16 10:45:52 -0600

    Add Sublime configuration

--- installer.sh
+++ installer.sh
@@ -1,2 +1,31 @@
-# Symlink BASH profile
-ln -Fs ~/dotfiles/.bash_profile ~/.bash_profile
+#	---------------------------------------------------------------------------
+#
+#	Description: Dotfiles install script
+#
+#	Sections:
+#	0. Variables
+#	1. BASH Profile Setup
+#	2. Sublime Setup
+#
+#	---------------------------------------------------------------------------
+
+
+#	-------------------------------
+#	0. Variables
+#	-------------------------------
+
+	currentUser=$(whoami)
+
+
+#	-------------------------------
+#	1. BASH Profile Setup
+#	-------------------------------
+
+	ln -Fs ~/dotfiles/.bash_profile ~/.bash_profile
+
+
+#	-------------------------------
+#	2. Sublime Setup
+#	-------------------------------
+
+	ln -Fs ~/dotfiles/sublime/Preferences.sublime-settings /Users/$currentUser/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
