907e72d2645a7764e538b2e230caa24bc43ef471
Taylor Daughtry <hello@taylordaughtry.com>
2018-02-24 14:34:41 -0600

    Symlink sublime to subl

--- installer.sh
+++ installer.sh
@@ -29,3 +29,5 @@
 #	-------------------------------
 
 	ln -Fs ~/dotfiles/sublime/Preferences.sublime-settings /Users/$currentUser/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+	sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
